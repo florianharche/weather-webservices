bede17d3333024e65bb6d035a04287efdc0d5567
Florian <florian.harche35@gmail.com>
2021-05-05 16:11:38 +0200

    dictionnaire

--- meteo.py
+++ meteo.py
@@ -11,23 +11,24 @@ urls = (
 
 app = web.application(urls, globals())
 
+
 class zipcode:
-    def __init__(self,params):
+    def __init__(self, params):
         if len(params) == 1:
             web.header('Content-Type', 'application/json')
             r = requests.get('api.openweathermap.org/data/2.5/weather?zip={zip code}&appid=REACT_APP_WEATHER_API_KEY',
                              auth=('USER', 'PASSWORD'))
-            data = r.json()
+            d = r.json()
             #dictionnaire
-            data = {
-                "temp"
-                "temp_min"
-                "temp_max"
-                "weather"
+            d = {
+                temp: data["temp"],
+                temp_min: data["temp_min"],
+                temp_max: data["temp_max"],
+                weather: data["weather"]
             }
         if len(params) != 1:
             raise Exception('Invalid ZIP code')
-        return json.dumps(data)
+        return json.dumps(d)
 
 
 if __name__ == "__main__":
