b9c786c63b96c9f08f0acb69dc4f1aec96c0c4a5
Florian <florian.harche35@gmail.com>
2021-05-05 12:29:59 +0200

    load dotenv

--- meteo.py
+++ meteo.py
@@ -3,6 +3,8 @@ import json
 import requests
 from dotenv import load_dotenv
 
+load_dotenv()
+
 urls = (
     '/(.*)', 'zipcode',
 )
@@ -13,14 +15,15 @@ class zipcode:
     def __init__(self,params):
         if len(params) == 1:
             web.header('Content-Type', 'application/json')
-            r = requests.get('api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid=REACT_APP_WEATHER_API_KEY' ,auth=('USER', 'PASSWORD'))
+            r = requests.get('api.openweathermap.org/data/2.5/weather?zip={zip code}&appid=REACT_APP_WEATHER_API_KEY',
+                             auth=('USER', 'PASSWORD'))
             data = r.json()
             #dictionnaire
             data = {
-                "temp" :
-                "temp_min" :
-                "temp_max" :
-                "weather" :
+                "temp"
+                "temp_min"
+                "temp_max"
+                "weather"
             }
         if len(params) != 1:
             raise Exception('Invalid ZIP code')
